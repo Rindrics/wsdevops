f861d6859f5f4118bc1655b2753bbeefde00e4d4
Akira Hayashi <smxshxishxad@me.com>
2020-01-21 15:23:22 +0900

    :sparkles: Add print_decimal()

--- R/my_range.R
+++ R/my_range.R
@@ -6,6 +6,13 @@ cat_endash <- function(x, y) {
   if (!is.character(x) || !is.character(y)) stop("Give me character args")
   paste(x, y, sep = "--")
 }
+
+#' Print numeric in float with given decimal points
+#'
+#' @param x Numeric vector to convert
+#' @param decimals Number of decimal points to print
+print_decimal <- function(x, decimals) {
+  sprintf(paste0("%.", decimals, "f"), x)
 }
 
 #' @export
